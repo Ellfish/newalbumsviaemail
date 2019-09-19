bbdf0b7fba8751230dc03e27c4337be5311a9b81
Elliot Fisher <elliot@icebergweb.com.au>
2019-09-19 18:04:33 +1000

    Added log4net logging. Fixed issue with ArtistAppService test code.

--- src/NewAlbums.Web/Startup.cs
+++ src/NewAlbums.Web/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NewAlbums.Artists;
 using NewAlbums.EntityFrameworkCore;
 using System.Reflection;
 
@@ -29,6 +31,9 @@ namespace NewAlbums.Web
 
             services.GenericServicesSimpleSetup<NewAlbumsDbContext>(Assembly.GetAssembly(typeof(BaseAppService)));
 
+            //NewsAlbums.Application services
+            services.AddTransient<IArtistAppService, ArtistAppService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
@@ -39,8 +44,10 @@ namespace NewAlbums.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddLog4Net();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
