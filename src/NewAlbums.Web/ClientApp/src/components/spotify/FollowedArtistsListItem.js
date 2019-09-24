68fcf4b7829ae9ebd56a5ea11aea08851367e00e
Elliot Fisher <elliot@icebergweb.com.au>
2019-09-24 20:09:50 +1000

    Added beginnings of allowing users to select which followed artists to subscribe to.

--- src/NewAlbums.Web/ClientApp/src/components/spotify/FollowedArtistsListItem.js
+++ src/NewAlbums.Web/ClientApp/src/components/spotify/FollowedArtistsListItem.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './FollowedArtistListItem.scss';
 
 export default function FollowedArtistsListItem(props) {
+    const [selected, setSelected] = useState(false);
+
+    let liClass = classNames({
+        'artist-container': true,
+        'selected': selected
+    });
 
     return (
-        <li className='artist-container'>
+        <li className={liClass} onClick={() => setSelected(!selected)}>
             <img className='artist-image' src={props.artist.image.url} loading='lazy' alt={props.artist.name} width='180' height='180' />
             <div className='artist-name'>{props.artist.name}</div>
         </li>
