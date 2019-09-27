c1474a0931c4db021e70ebacf75cc0a316a47c36
Elliot Fisher <elliot@icebergweb.com.au>
2019-09-27 15:32:08 +1000

    Improved ErrorMessage display.

--- src/NewAlbums.Web/ClientApp/src/components/common/ErrorMessage.js
+++ src/NewAlbums.Web/ClientApp/src/components/common/ErrorMessage.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './ErrorMessage.scss';
 
 export default function ErrorMessage(props) {
+    let message = props.message;
+
+    if (message && message.toLowerCase().indexOf('error') !== 0) {
+        message = 'Error: ' + message;
+    }
+
     return (
         <div className='error-message-container'>
             <div className='error-message'>
-                {props.message}
+                {message}
             </div>
         </div>
     );    
