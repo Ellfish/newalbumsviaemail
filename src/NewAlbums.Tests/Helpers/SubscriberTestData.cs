8300359d770b3d256ddf3881f21e61386ebf7dee
Elliot Fisher <elliot@icebergweb.com.au>
2019-10-13 11:20:21 +1100

    Added beginning of unit test project.

---
+++ src/NewAlbums.Tests/Helpers/SubscriberTestData.cs
@@ -0,0 +1,74 @@
+﻿using NewAlbums.Artists;
+using NewAlbums.EntityFrameworkCore;
+using NewAlbums.Subscribers;
+using NewAlbums.Subscriptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewAlbums.Tests.Helpers
+{
+    public static class SubscriberTestData
+    {
+        private static readonly DateTime _createdDate = DateTime.UtcNow;
+
+        public static void SeedDatabaseSubscriberToTwoArtists(this NewAlbumsDbContext context)
+        {
+            var artists = CreateTwoArtists();
+            context.Artists.AddRange(artists);
+
+            var subscriber = CreateSubscriber();
+            context.Subscribers.Add(subscriber);
+
+            CreateSubscriptions(subscriber, artists);
+
+            context.SaveChanges();
+        }
+
+        private static void CreateSubscriptions(Subscriber subscriber, List<Artist> artists)
+        {
+            if (subscriber.Subscriptions == null)
+                subscriber.Subscriptions = new List<Subscription>();
+
+            foreach (var artist in artists)
+            {
+                subscriber.Subscriptions.Add(new Subscription
+                {
+                    Artist = artist,
+                    CreatedDate = _createdDate,
+                    Subscriber = subscriber
+                });
+            }
+        }
+
+        private static Subscriber CreateSubscriber()
+        {
+            return new Subscriber
+            {
+                CreatedDate = _createdDate,
+                EmailAddress = "subscriber@newalbumsvia.email"
+            };
+        }
+
+        private static List<Artist> CreateTwoArtists()
+        {
+            var artists = new List<Artist>();
+
+            artists.Add(new Artist
+            {
+                CreatedDate = _createdDate,
+                Name = "Crowded House",
+                SpotifyId = "7ohlPA8dRBtCf92zaZCaaB"
+            });
+
+            artists.Add(new Artist
+            {
+                CreatedDate = _createdDate,
+                Name = "Nils Frahm",
+                SpotifyId = "5gqhueRUZEa7VDnQt4HODp"
+            });
+
+            return artists;
+        }
+    }
+}
---
+++ src/NewAlbums.Tests/Subscribers/SubscriberAppServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using NewAlbums.EntityFrameworkCore;
+using NewAlbums.Subscribers;
+using NewAlbums.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GenericServices;
+using GenericServices.PublicButHidden;
+using GenericServices.Setup;
+using TestSupport.EfHelpers;
+using Xunit;
+using NewAlbums.Artists;
+using NewAlbums.Subscriptions;
+using Xunit.Extensions.AssertExtensions;
+using NewAlbums.Subscribers.Dto;
+using NewAlbums.Artists.Dto;
+using NewAlbums.Subscriptions.Dto;
+
+namespace NewAlbums.Tests.Subscribers
+{
+    public class SubscriberAppServiceTest
+    {
+        private readonly ISubscriberAppService _subscriberAppService;
+
+        [Fact]
+        public void EmailSentForNewAlbum()
+        {
+            //Setup
+            var options = SqliteInMemory.CreateOptions<NewAlbumsDbContext>();
+            using (var context = new NewAlbumsDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.SeedDatabaseSubscriberToTwoArtists();
+
+                var utData = context.SetupSingleDtoAndEntities<SubscriberDto>();
+                utData.AddSingleDto<ArtistDto>();
+                utData.AddSingleDto<SubscriptionDto>();
+
+                var service = new CrudServices(context, utData.ConfigAndMapper);
+
+                //_subscriberAppService = new SubscriberAppService(service, )
+
+                //Attempt
+                var dto = service.ReadSingle<Subscription>(1L);
+
+                //Verify
+                service.IsValid.ShouldBeTrue(service.GetAllErrors());
+                dto.Id.ShouldEqual(1);
+
+            }
+        }
+    }
+}
