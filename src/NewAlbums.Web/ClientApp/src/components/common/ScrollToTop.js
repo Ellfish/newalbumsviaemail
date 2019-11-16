a4c910b1ca87f26d536bc1e96fd3b5c803b6009c
Elliot Fisher <elliot@icebergweb.com.au>
2019-11-16 17:57:18 +1100

    Implemented scroll to top upon route change.

---
+++ src/NewAlbums.Web/ClientApp/src/components/common/ScrollToTop.js
@@ -0,0 +1,25 @@
+﻿//From: https://reacttraining.com/react-router/web/guides/scroll-restoration
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        let top = 0;
+        let header = document.querySelector('header');
+        if (header) {
+            top = header.offsetHeight;
+        }
+
+        //We have smoothscroll-polyfill to cover browsers which don't support "behavior"
+        window.scrollTo({
+            top: top,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }, [pathname]);
+
+    return null;
+}
--- src/NewAlbums.Web/ClientApp/src/index.js
+++ src/NewAlbums.Web/ClientApp/src/index.js
@@ -3,18 +3,23 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/common/ScrollToTop';
 import App from './App';
 import { unregister } from './registerServiceWorker';
+import smoothscroll from 'smoothscroll-polyfill';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <BrowserRouter basename={baseUrl}>
+        <ScrollToTop />
+        <App />
+    </BrowserRouter>,
+    rootElement);
 
 //Don't want the service worker, causing too many caching issues
 //registerServiceWorker();
 unregister();
+
+smoothscroll.polyfill();
